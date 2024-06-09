4bbc424a50dda9ff0d7a14ccf856938a7f3113a9
lsf2008 <limax_2008@outlook.com>
2024-06-09 13:30:38 +0800

    更新说明

--- T.py
+++ T.py
@@ -23,11 +23,10 @@ parser.add_argument('--learning_rate_ped2', type=float, default=0.001, help='ini
 parser.add_argument('--learning_rate_avenue', default=0.0000001, type=float, help='initial learning_rate')
 parser.add_argument('--num_workers', type=int, default=0, help='number of workers for the train loader')
 parser.add_argument('--num_workers_test', type=int, default=0, help='number of workers for the test loader')
-parser.add_argument('--loss_m_weight', help='loss_m_weight', type=float, default=0.0002)
+
 
 parser.add_argument('--dataset_type', type=str, default='ped2', choices=['ped2', 'avenue', 'shanghai'],
                     help='type of dataset: ped2, avenue, shanghai')
-# parser.add_argument('--path', type=str, default='./exp_up13', help='directory of data')
 parser.add_argument('--path', type=str, default='exp_up', help='directory of data')
 parser.add_argument('--path_num', type=int, default=0, help='number of path')
 parser.add_argument('--mem_dim', type=int, default=2000, help='size of mem')
@@ -101,27 +100,12 @@ exp_dir += '_lr' + str(learning_rate) + '_'
 exp_dir += 'weight'
 exp_dir += '_recon'
 
-
-
-# print('exp_dir: ', exp_dir)
-
-
-# torch.backends.cudnn.enabled = True  # make sure to use cudnn for computational performance
-
-
-# train_folder = os.path.join(args.dataset_path, args.dataset_type, 'training')
-# train_folder = os.path.join('UCSDped2', 'Train')
-# train_folder = os.path.join('Avenue', 'Train')
-
 print(f'train_folder:{train_folder}')
 
 trans_compose = transforms.Compose([transforms.ToPILImage(), transforms.RandomRotation(degrees=(90, 90),
                                                                                        expand=False, center=(128, 128)),
                                                                              transforms.ToTensor()])
 
-# trans_compose = transforms.Compose([transforms.ToTensor()])
-
-# trans_compose = transforms.Compose([transforms.ToTensor(), transforms.RandomHorizontalFlip(p=0.8)])
 
 # Loading dataset
 img_extension = '.tif' if args.dataset_type == 'ped2' else '.jpg'
@@ -133,8 +117,6 @@ train_dataset_jump = Reconstruction3DDataLoaderJump(train_folder,
                                                     trans_compose,
                                                 resize_height=args.h, resize_width=args.w, dataset=args.dataset_type,
                                                 jump=args.jump, img_extension=img_extension)
-
-#train_dataset = traindataset(train_folder)
 
 
 train_batch = data.DataLoader(train_dataset, batch_size=args.batch_size,
@@ -150,10 +132,6 @@ print(f'len(train_batch):{len(train_batch)}')
 
 
 log_dir = os.path.join('./', args.path + str(args.path_num), args.dataset_type, exp_dir)
-# while os.path.exists(log_dir):
-#     args.path_num = args.path_num + 1
-# log_dir = os.path.join('./', args.path + str(args.path_num), args.dataset_type, exp_dir)
-# if not os.path.exists(args.path + str(args.path_num)):
 
 print(f'log_dir:{log_dir}')
 if not os.path.exists(log_dir):
@@ -168,8 +146,7 @@ torch.set_printoptions(profile="full")
 
 loss_func_mse = nn.MSELoss(reduction='none')
 
-# entropy_loss_func = EntropyLossEncap().cuda()
-# loss_m_weight = args.loss_m_weight
+
 
 if args.start_epoch < args.epochs:
     model = ML_MemAE_SC(num_in_ch=channel_in, features_root=32,
@@ -179,8 +156,7 @@ if args.start_epoch < args.epochs:
     model.cuda()
     separateness_loss_ano = torch.zeros(1, 1).cuda()
     compactness_loss_ano = torch.zeros(1, 1).cuda()
-    # separateness_loss = torch.zeros(1, 1).cuda()
-    # compactness_loss = torch.zeros(1, 1).cuda()
+
     sigma = args.sigma_noise ** 2
 
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
@@ -194,15 +170,10 @@ if args.start_epoch < args.epochs:
         pseudolosscounter = 0
         losscounter = 0
 
-        # for j, (imgs) in enumerate(zip(train_batch)):
-        # for j, (imgs) in enumerate(train_batch):
-        #     net_in = copy.deepcopy(imgs)
-        #     net_in = net_in.cuda()
         for j, (imgs, imgsjump) in enumerate(zip(train_batch, train_batch_jump)):
             net_in = copy.deepcopy(imgs)
             net_in = net_in.cuda()
             imgsjump_gaus = gaussian(imgsjump, 1, 0, sigma)
-            # noise = np.random.normal(0, 1, imgsjump.shape)
             jump_pseudo_stat = []
             cls_labels = []
 
@@ -216,7 +187,6 @@ if args.start_epoch < args.epochs:
                 total_pseudo_prob += args.pseudo_anomaly_jump
                 if pseudo_anomaly_jump:
                     net_in[b] = imgsjump_gaus[b][0]
-                    # net_in[b] = imgsjump[b][0]
                     jump_pseudo_stat.append(True)
                     pseudo_bool = True
                 else:
@@ -234,10 +204,6 @@ if args.start_epoch < args.epochs:
             # out = model(net_in)
             loss_mem = torch.abs(out['mem'].cuda() - out['mem_ano'].cuda()) * -1.0
             loss_mse = loss_func_mse(out["recon"], net_in)
-            # cross_entropy = entropy_loss_func(out["att"])
-            # loss_sparsity = torch.mean(torch.sum(-out["att_weight3"] * torch.log(out["att_weight3"] + 1e-12), dim=1))
-            # loss_sparsity_ano = -1.0 * torch.mean(
-            #     torch.sum(-out["att_weight3_ano"] * torch.log(out["att_weight3_ano"] + 1e-12), dim=1))
 
             modified_loss_mse = []
             for b in range(args.batch_size):
@@ -254,10 +220,9 @@ if args.start_epoch < args.epochs:
             stacked_loss_mse = torch.stack(modified_loss_mse)
             loss_all = (stacked_loss_mse
                         + args.loss_separate * separateness_loss + args.loss_compact * compactness_loss
-                        # + args.loss_m_weight * loss_sparsity_ano + args.loss_m_weight * loss_sparsity
                         + args.loss_separate * separateness_loss_ano + args.loss_compact * compactness_loss_ano
                         ).sum() + loss_mem.sum() * 0.00002
-            # loss_all = loss_mse
+
 
             optimizer.zero_grad()
             loss_all.backward()
@@ -269,16 +234,11 @@ if args.start_epoch < args.epochs:
         # Save the model and the memory items
         model_dict = {
             'model': model,
-            # 'discriminator':discriminator,
-            # 'optimizer_g': optimizer_g.state_dict(),
-            # 'optimizer_d': optimizer_d.state_dict(),
         }
 
         torch.save(model_dict, os.path.join(log_dir, 'model_{:02d}.pth'.format(epoch)))
     print('Training is finished')
     toc = time.time()
-    # print('time:' + str(1000 * (toc - tic)) + "ms")
-    # print('mean time:' + str(1000 * (toc - tic) / args.epochs) + "ms")
     print('time:' + str(1000 * (toc - tic) / 60000 / 60) + "h")
     print('mean time:' + str(1000 * (toc - tic) / 60000 / args.epochs) + "min")
     sys.stdout = orig_stdout
@@ -289,10 +249,9 @@ if args.start_epoch < args.epochs:
 #Test
 loss_func_mse = nn.MSELoss(reduction='none')
 labels = np.load('./frame_labels_'+args.dataset_type+'.npy', allow_pickle=True)
-# img_extension = '.tif' if args.dataset_type == 'ped2' else '.jpg'
 test_dataset = Reconstruction3DDataLoader(test_folder, trans_compose,
                                           resize_height=args.h, resize_width=args.w, dataset=args.dataset_type,
-                                          img_extension=img_extension, train=False)
+                                          img_extension=img_extension)
 test_size = len(test_dataset)
 
 test_batch = data.DataLoader(test_dataset, batch_size=args.test_batch_size,
@@ -305,11 +264,9 @@ model = ML_MemAE_SC(num_in_ch=channel_in, features_root=32,
 print(f'len(test_batch):{len(test_batch)}')
 videos = OrderedDict()
 videos_list = sorted(glob.glob(os.path.join(test_folder, '*/')))
-# videos_list = sorted(glob.glob('test_folder/*'))
-# print(videos_list)
+
 for video in videos_list:
     video_name = video.split('\\')[-2]
-    #video_name = video.split('/')[-2]
     videos[video_name] = {}
     videos[video_name]['path'] = video
     videos[video_name]['frame'] = glob.glob(os.path.join(video, '*' + img_extension))
@@ -331,8 +288,6 @@ for epoch_num in range(args.epochs):
         model_dict = torch.load(log_dir + "\\" + f'model_0{epoch_num}.pth')
     else:
         model_dict = torch.load(log_dir + "\\" + f'model_{epoch_num}.pth')
-
-    # model_weight = model_dict['model']
     model.load_state_dict(model_dict['model'].state_dict())
 
     model.cuda()
@@ -364,18 +319,11 @@ for epoch_num in range(args.epochs):
             loss_mse = loss_func_mse(outputs[0, :, 8], imgs[0, :, 8])
             mse_feas = compactness_loss.item()
 
-            # Calculating the threshold for updating at the test time
-            # point_sc = point_score(outputs, imgs)
 
         loss_pixel = torch.mean(loss_mse)
         mse_imgs = loss_pixel.item()
-        # if point_sc < args.th:
-        #     query = F.normalize(feas, dim=1)
-        #     query = query.permute(0, 2, 3, 4, 1)  # b X deep X h X w X d
-        #     model.mem3 = model.update(query, out['mem'], False).cuda()
-
         psnr_list[videos_list[video_num].split('\\')[-2]].append(psnr(mse_imgs))
-        # feature_distance_list[videos_list[video_num].split('\\')[-2]].append(mse_feas)
+
 
     # Measuring the abnormality score (S) and the AUC
     anomaly_score_total_list = []
@@ -383,16 +331,11 @@ for epoch_num in range(args.epochs):
     for vi, video in enumerate(sorted(videos_list)):
         video_name = video.split('\\')[-2]
         score = anomaly_score_list(psnr_list[video_name])
-        # anomaly_score_total_list += score_sum(anomaly_score_list(psnr_list[video_name]),
-        #                                       feature_distance_list[video_name], args.alpha)
         anomaly_score_total_list += score
         vid_idx += [vi for _ in range(len(score))]
 
     accuracy = AUC(anomaly_score_total_list, np.expand_dims(1 - labels_list, 0))
 
-    # print('vididx,frame,anomaly_score,anomaly_label')
-    # for a in range(len(anomaly_score_total_list)):
-    #     print(str(vid_idx[a]), ',', str(a), ',', 1-anomaly_score_total_list[a], ',', labels_list[a])
 
     print('The result of ', args.dataset_type)
     if epoch_num < 10:
@@ -401,8 +344,6 @@ for epoch_num in range(args.epochs):
         print(f'model_{epoch_num}_AUC: ', accuracy * 100, '%')
     print('----------------------------------------')
 toc = time.time()
-# print('time:' + str(1000 * (toc - tic)) + "ms")
-# print('mean time:' + str(1000 * (toc - tic) / args.epochs) + "ms")
 print('time:' + str(1000 * (toc - tic) / 3600000) + "h")
 print('mean time:' + str(1000 * (toc - tic) / 60000 / args.epochs) + "min")
 print('Testing is finished')
--- updatemem.py
+++ updatemem.py
@@ -6,125 +6,6 @@ from basic_modules import *
 import copy
 
 
-# relu based hard shrinkage function, only works for positive values
-def hard_shrink_relu(input, lambd=0., epsilon=1e-12):
-    output = (F.relu(input - lambd) * input) / (torch.abs(input - lambd) + epsilon)     #大于0，硬收缩
-    return output
-
-
-class Memory(nn.Module):
-    def __init__(self, mem_dim, fea_dim, shrink_thres=0.0025, choices=None, hard_shrink=True):
-        super(Memory, self).__init__()
-        self.mem_dim = mem_dim   #插槽
-        self.fea_dim = fea_dim
-        self.choice = choices
-        self.hard_shrink = hard_shrink
-        self.memMatrix = nn.Parameter(torch.empty(mem_dim, fea_dim))
-        self.shrink_thres = shrink_thres
-
-        self.reset_parameters()
-
-    def reset_parameters(self):
-        stdv = 1. / math.sqrt(self.memMatrix.size(1))
-        self.memMatrix.data.uniform_(-stdv, stdv)      # 随机化参数
-
-    def get_update_query(self, mem, max_indices, score, query, train):
-
-        m, d = mem.size()
-        if train:
-            query_update = torch.zeros((m, d)).cuda()
-            random_update = torch.zeros((m, d)).cuda()
-            for i in range(m):
-                idx = torch.nonzero(max_indices.squeeze(1) == i)
-                a, _ = idx.size()
-                # ex = update_indices[0][i]
-                if a != 0:
-                    # random_idx = torch.randperm(a)[0]
-                    # idx = idx[idx != ex]
-                    #                     query_update[i] = torch.sum(query[idx].squeeze(1), dim=0)
-                    query_update[i] = torch.sum(((score[idx, i] / torch.max(score[:, i])) * query[idx].squeeze(1)),
-                                                dim=0)
-                    # random_update[i] = query[random_idx] * (score[random_idx,i] / torch.max(score[:,i]))
-                else:
-                    query_update[i] = 0
-                    # random_update[i] = 0
-
-            return query_update
-
-        else:
-            query_update = torch.zeros((m, d)).cuda()
-            for i in range(m):
-                idx = torch.nonzero(max_indices.squeeze(1) == i)
-                a, _ = idx.size()
-                # ex = update_indices[0][i]
-                if a != 0:
-                    # idx = idx[idx != ex]
-                    query_update[i] = torch.sum(((score[idx, i] / torch.max(score[:, i])) * query[idx].squeeze(1)),
-                                                dim=0)
-                #                     query_update[i] = torch.sum(query[idx].squeeze(1), dim=0)
-                else:
-                    query_update[i] = 0
-
-            return query_update
-
-
-    def update(self, query, keys, train=True):
-
-        softmax_score_query, softmax_score_memory = self.get_score(keys, query)
-
-        _, gathering_indices = torch.topk(softmax_score_memory, 1, dim=1)
-        _, updating_indices = torch.topk(softmax_score_query, 1, dim=0)
-
-        if train:
-            # top-1 queries (of each memory) update (weighted sum) & random pick
-            query_update = self.get_update_query(keys, gathering_indices, softmax_score_query,
-                                                 query, train)
-
-        else:
-            # only weighted sum update when test
-            query_update = self.get_update_query(keys, gathering_indices, softmax_score_query,
-                                                 query, train)
-
-        # top-1 update
-        # query_update = query_reshape[updating_indices][0]
-        # updated_memory = F.normalize(query_update + keys, dim=1)
-        return query_update
-
-    def get_score(self, mem, query):
-        # bs, deep, h, w, d = query.size()
-        m, d = mem.size()
-
-        score = torch.matmul(query, torch.t(mem))  # b X deep X h X w X m
-        score = score.view(-1, m)  # (b X deep X h X w) X m
-
-        score_query = F.softmax(score, dim=0)
-        score_memory = F.softmax(score, dim=1)
-        return score_query, score_memory
-
-    def forward(self, x):
-        """
-        :param x: query features with size [N,C], where N is the number of query items,
-                  C is same as dimension of memory slot
-
-        :return: query output retrieved from memory, with the same size as x.
-        """
-        att_weight = F.linear(input=x, weight=self.memMatrix)  # [N,C] by [M,C]^T --> [N,M] 线性变换
-        att_weight = F.softmax(att_weight, dim=1)  # NxM
-
-        # if use hard shrinkage
-        if self.shrink_thres > 0 and self.hard_shrink:
-            att_weight = hard_shrink_relu(att_weight, lambd=self.shrink_thres)  # [N,M]
-            # normalize
-            att_weight = F.normalize(att_weight, p=1, dim=1)  # [N,M]
-
-        # out slot
-        out = F.linear(att_weight, self.memMatrix.permute(1, 0))  # [N,M] by [M,C]  --> [N,C] N是查询项的数目，C与记忆槽的维数相同
-
-        return dict(out=out, att_weight=att_weight, mem=self.memMatrix)
-
-        # return dict(output=out, att=att_weight)
-
-
 class ML_MemAE_SC(nn.Module):
     def __init__(self, num_in_ch, features_root, mem_dim, ano_mem_dim, shrink_thres, mem_usage, skip_ops, hard_shrink_opt):
         super(ML_MemAE_SC, self).__init__()
@@ -138,7 +19,6 @@ class ML_MemAE_SC(nn.Module):
         self.skip_ops = skip_ops
         self.mem3 = nn.Parameter(torch.empty(mem_dim, 256))
         self.mem3_ano = nn.Parameter(torch.empty(mem_dim, 256))
-        # self.mem3 = F.normalize(torch.rand((mem_dim, 256), dtype=torch.float), dim=1).cuda()
         self.reset_parameters()
 
 
@@ -151,19 +31,10 @@ class ML_MemAE_SC(nn.Module):
         self.down_2 = down(features_root * 2, features_root * 4)
         self.down_3 = down(features_root * 4, features_root * 8)
 
-        # memory modules
-        # self.mem3 = Memory(mem_dim=self.mem_dim, fea_dim=features_root * 8, #* 2 * 32 * 32,
-        #                    shrink_thres=self.shrink_thres, hard_shrink=self.hard_shrink_opt) if self.mem_usage[3] else None
-        # self.mem3_ano = Memory(mem_dim=self.ano_mem_dim, fea_dim=features_root * 8,  # * 2 * 32 * 32,
-        #                    shrink_thres=self.shrink_thres, hard_shrink=self.hard_shrink_opt) if self.mem_usage[3] else None
-        # self.memory = Memory_up(memory_size=10, feature_dim=256, key_dim=512, temp_update=0.1, temp_gather=0.1)
-
-        # self.up_4 = outconv(features_root * 16, features_root * 8)
         self.out_conv0 = outconv(features_root * 16, features_root * 8)
         self.up_3 = up(features_root * 8, features_root * 4, op=self.skip_ops[-1])
         self.up_2 = up(features_root * 4, features_root * 2, op=self.skip_ops[-2])
         self.up_1 = up(features_root * 2, features_root, op=self.skip_ops[-3])
-        # self.out_conv = outconv(features_root, num_in_ch)
         self.out_conv1 = outconv(features_root, num_in_ch)
 
     def reset_parameters(self):
@@ -192,17 +63,11 @@ class ML_MemAE_SC(nn.Module):
             for i in range(m):
                 idx = torch.nonzero(max_indices.squeeze(1) == i)
                 a, _ = idx.size()
-                # ex = update_indices[0][i]
                 if a != 0:
-                    # random_idx = torch.randperm(a)[0]
-                    # idx = idx[idx != ex]
-                    #                     query_update[i] = torch.sum(query[idx].squeeze(1), dim=0)
                     query_update[i] = torch.sum(((score[idx, i] / torch.max(score[:, i])) * query[idx].squeeze(1)),
                                                 dim=0)
-                    # random_update[i] = query[random_idx] * (score[random_idx,i] / torch.max(score[:,i]))
                 else:
                     query_update[i] = 0
-                    # random_update[i] = 0
 
             return query_update
 
@@ -211,12 +76,9 @@ class ML_MemAE_SC(nn.Module):
             for i in range(m):
                 idx = torch.nonzero(max_indices.squeeze(1) == i)
                 a, _ = idx.size()
-                # ex = update_indices[0][i]
                 if a != 0:
-                    # idx = idx[idx != ex]
                     query_update[i] = torch.sum(((score[idx, i] / torch.max(score[:, i])) * query[idx].squeeze(1)),
                                                 dim=0)
-                #                     query_update[i] = torch.sum(query[idx].squeeze(1), dim=0)
                 else:
                     query_update[i] = 0
 
@@ -249,10 +111,6 @@ class ML_MemAE_SC(nn.Module):
             query_update = self.get_update_query(keys, gathering_indices, updating_indices, softmax_score_query,
                                                  query_reshape, train)
             updated_memory = F.normalize(query_update + keys, dim=1)
-
-        # top-1 update
-        # query_update = query_reshape[updating_indices][0]
-        # updated_memory = F.normalize(query_update + keys, dim=1)
 
 
         return updated_memory.detach()
@@ -356,7 +214,7 @@ class ML_MemAE_SC(nn.Module):
             # update
             updated_memory_ano = self.update(query, self.mem3_ano, train)
 
-            # return updated_query, updated_memory, softmax_score_query, softmax_score_memory, gathering_loss, spreading_loss
+
 
         # test
         else:
@@ -371,10 +229,8 @@ class ML_MemAE_SC(nn.Module):
             # update
             updated_memory = self.mem3
 
-            # return updated_query, updated_memory, softmax_score_query, softmax_score_memory, gathering_loss
 
         recon = self.out_conv0(updated_query)
-        # recon = self.up_3(x3, x2 if self.skip_ops[-1] != "none" else None)
         recon = self.up_3(recon, x2 if self.skip_ops[-1] != "none" else None)
         recon = self.up_2(recon, x1 if self.skip_ops[-2] != "none" else None)
         recon = self.up_1(recon, x0 if self.skip_ops[-3] != "none" else None)
